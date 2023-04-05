24287ff09378f4731384edff18a9ead35fe31311
madrimovDev <madrimov5014@gmail.com>
2023-04-05 15:36:06 +0500

    asd

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { join } from 'path';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'http://localhost:3000',
     credentials: true,
   });
   app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
