2b9ed9a96ff1cf83ad298ca4a4d971d86368dab1
madrimovDev <madrimov5014@gmail.com>
2023-04-05 18:25:49 +0500

    cors 2

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
     origin: [
       'https://madrimov.uz',
       'http://localhost:5173',
-      'http://localhost:5173',
+      'http://localhost:5174',
     ],
     credentials: true,
     allowedHeaders: '*',
